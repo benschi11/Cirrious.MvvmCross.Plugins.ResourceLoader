36714cffa3de3c801c66b72f166488dd81cc94db
Benedikt Neuhold <benedikt.neuhold@live.at>
2015-09-02 22:47:37 +0200

    Forked Repo from MvvmCross

---
+++ Cirrious.MvvmCross.Plugins.ResourceLoader.WindowsUWP/MvxWindowsResourceLoader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+
+namespace Cirrious.MvvmCross.Plugins.ResourceLoader.WindowsUWP
+{
+    public class MvxStoreResourceLoader : MvxResourceLoader
+    {
+        public override async void GetResourceStream(string resourcePath, Action<Stream> streamAction)
+        {
+            // we needed to replace the "/" with "\\" - see https://github.com/slodge/MvvmCross/issues/332
+            resourcePath = resourcePath.Replace("/", "\\");
+            var file = await Package.Current.InstalledLocation.GetFileAsync(resourcePath);
+            var streamWithContent = await file.OpenReadAsync();
+            var stream = streamWithContent.AsStreamForRead();
+            streamAction(stream);
+        }
+    }
+}
---
+++ Cirrious.MvvmCross.Plugins.ResourceLoader.WindowsUWP/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.Platform;
+using Cirrious.CrossCore.Plugins;
+
+namespace Cirrious.MvvmCross.Plugins.ResourceLoader.WindowsUWP
+{
+    public class Plugin : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterType<IMvxResourceLoader, MvxStoreResourceLoader>();
+        }
+    }
+}
